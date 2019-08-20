a491310e4feaa858f7afa1bcedb56fb016050e0e
FransEkstrand <frans.ekstrand@gmail.com>
2019-08-20 13:21:23 +0300

    added Hello-Name

---
+++ Hello-Name/script.js
@@ -0,0 +1,25 @@
+let input = document.getElementById('name');
+let button = document.getElementById('button');
+
+button.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+    alert('Hello ' + input.value);
+
+
+    if {
+        onButtonClick = "Please enter name";
+    }
+
+}
+
+
+
+
+// Denhär är en förkortad Verision av den
+
+
+
+// function onButtonClick() {
+//     alert("Hello " + document.getElementById("name").value + "!")
+// }
